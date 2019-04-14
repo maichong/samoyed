c9d563251946364dc96c88cb83574463eb399ca1
Liang Xingchen <liang@maichong.it>
2019-04-15 00:25:16 +0800

    浏览器环境检查

--- packages/app/index.d.ts
+++ packages/app/index.d.ts
@@ -6,11 +6,39 @@ export interface Defaults {
   animationDuration: number;
 }
 
+export interface Environments {
+  mac?: boolean;
+  windows?: boolean;
+  linux?: boolean;
+  android?: boolean;
+  ios?: boolean;
+  iphone?: boolean;
+  ipad?: boolean;
+  phone?: boolean;
+  desktop?: boolean;
+  tablet?: boolean;
+  webkit?: boolean;
+  gecko?: boolean;
+  chrome?: boolean;
+  firefox?: boolean;
+  safari?: boolean;
+  edge?: boolean;
+  ie?: boolean;
+  ie8?: boolean;
+  ie9?: boolean;
+  ie10?: boolean;
+  ie11?: boolean;
+  landscape?: boolean;
+  portrait?: boolean;
+  touch?: boolean;
+}
+
 export class App {
   defaults: Defaults;
   views: {
     [name: string]: React.ComponentClass<any>;
   };
+  is: Environments;
 
   init(): void;
 }
--- packages/app/index.js
+++ packages/app/index.js
@@ -7,10 +7,86 @@ class App {
             animationDuration: 300,
             switchAnimationDuration: 300,
         };
+        let ua = window.navigator.userAgent;
+        let is = {
+            mac: false,
+            windows: /Windows NT|Windows Phone/i.test(ua),
+            linux: false,
+            ios: false,
+            android: false,
+            iphone: false,
+            ipad: false,
+            phone: false,
+            tablet: false,
+            desktop: false,
+            webkit: false,
+            gecko: /Gecko\/\d+/i.test(ua),
+            chrome: false,
+            firefox: /Firefox/i.test(ua),
+            safari: false,
+            edge: /Edge/.test(ua),
+            ie: /MSID|Trident/i.test(ua),
+            ie8: false,
+            ie9: false,
+            ie10: false,
+            ie11: false,
+            landscape: window.innerWidth >= window.innerHeight,
+            portrait: window.innerWidth < window.innerHeight,
+        };
+        is.ipad = !is.ie && /iPad/i.test(ua);
+        is.iphone = !is.ie && /iPhone/i.test(ua);
+        is.android = !is.ie && /Android/i.test(ua);
+        is.chrome = is.android || (!is.ie && !is.edge && /Chrome/i.test(ua));
+        is.webkit = !is.ie && !is.edge && /Webkit/i.test(ua);
+        is.safari = !is.ie && !is.edge && !is.firefox && !is.chrome && !is.android && /Safari/.test(ua);
+        is.ios = is.iphone || is.ipad;
+        is.desktop = !is.ios && !is.android && !/Windows Phone/i.test(ua);
+        is.mac = is.desktop && /Mac OS/i.test(ua);
+        is.linux = !is.android && /Linux/i.test(ua);
+        is.tablet = is.ipad || (is.android && !/Mobile/.test(ua));
+        is.phone = !is.desktop && !is.tablet;
+        if (is.ie) {
+            if (/MSIE 8/.test(ua)) {
+                is.ie8 = true;
+            }
+            else if (/MSIE 9/.test(ua)) {
+                is.ie9 = true;
+            }
+            else if (/MSIE 10/.test(ua)) {
+                is.ie10 = true;
+            }
+            else if (/rv\:11/.test(ua)) {
+                is.ie11 = true;
+            }
+        }
+        this.is = is;
+        if (is.desktop) {
+            window.addEventListener('resize', () => {
+                let landscape = window.innerWidth >= window.innerHeight;
+                if (landscape === is.landscape)
+                    return;
+                is.landscape = landscape;
+                is.portrait = !landscape;
+                const classList = window.document.body.classList;
+                if (landscape) {
+                    classList.remove('s-portrait');
+                    classList.add('s-landscape');
+                }
+                else {
+                    classList.remove('s-landscape');
+                    classList.add('s-portrait');
+                }
+            });
+        }
     }
     init() {
         const body = window.document.body;
         body.classList.add('powered-by-samoyed', 'maichong-software', 'https://maichong.it', 's-samoyed');
+        for (let key in this.is) {
+            if (this.is[key]) {
+                body.classList.add(`s-${key}`);
+            }
+        }
     }
 }
 exports.App = App;
