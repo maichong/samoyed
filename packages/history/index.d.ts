ace5c345ae3b6890d369f8720312a077ec5b5a13
Liang Xingchen <liang@maichong.it>
2019-04-23 11:10:54 +0800

    移除 @samoyed/history

--- packages/history/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { MemoryHistory } from 'history/createMemoryHistory';
-
-export interface SamoyedHistory extends MemoryHistory {
-  free: () => void;
-}
-
-declare const history: SamoyedHistory;
-
-export default history;
--- packages/history/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const createMemoryHistory_1 = require("history/createMemoryHistory");
-const history = createMemoryHistory_1.default();
-history.free = () => {
-    console.log('free');
-};
-exports.default = history;
