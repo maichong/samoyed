11db433f913d1ed9d60714d701358f0d1a1005da
Liang Xingchen <liang@maichong.it>
2019-06-20 12:29:39 +0800

    ts bug

--- packages/redux/device.d.ts
+++ packages/redux/device.d.ts
@@ -1,4 +1,4 @@
 import { Action } from 'redux-actions';
 import { ApplyDevicePayload } from '.';
 
-export function deviceSaga(action: Action<LoadDetailPayload>): any;
+export function deviceSaga(action: Action<ApplyDevicePayload>): any;
